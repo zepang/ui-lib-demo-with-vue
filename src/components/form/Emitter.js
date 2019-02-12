2d6e37cec5dbf55eb5dff6f3b513e4f6f25f6d1f
zepang <ummlq4@gmail.com>
2019-02-12 11:17:04 +0800

    添加查找父组件子组件兄弟组件的工具函数

--- src/components/form/Emitter.js
+++ src/components/form/Emitter.js
@@ -1,23 +1,4 @@
-function findParent (comp, compName) {
-  let parent = comp.$parent 
-  if (!parent) return undefined
-  if (parent.$options.name === compName) {
-    return parent
-  } else {
-    return findParent(parent, compName)
-  }
-}
-function findChild (comp, compName) {
-  let children = comp.$children
-  if (!comp.$children || !comp.$children.length) return undefined
-  for (let child of children) {
-    if (child.$options.name === compName) {
-      return child
-    } else {
-      return findChild(child, compName)
-    }
-  }
-}
+import { findParent, findChild } from '../../utils'
 export default {
   methods: {
     broadcase (compName, eName, params) {
--- src/utils/index.js
+++ src/utils/index.js
@@ -4,3 +4,56 @@ export function camlToParam (str) {
   })
   return newStr
 }
+
+export function findParent (comp, compName) {
+  let parent = comp.$parent 
+  if (!parent) return undefined
+  if (parent.$options.name === compName) {
+    return parent
+  } else {
+    return findParent(parent, compName)
+  }
+}
+
+export function findParents (comp, compName) {
+  let parents = []
+  let parent = comp.$parent
+  if (parent) {
+    if (parent.$options.name === compName) parents.push(parent)
+    parents.concat(findParents(parent, compName))
+  }
+  return parents
+}
+
+export function findChild (comp, compName) {
+  let children = comp.$children
+  if (!children || !children.length) return undefined
+  for (let child of children) {
+    if (child.$options.name === compName) {
+      return child
+    } else {
+      return findChild(child, compName)
+    }
+  }
+}
+
+export function findChildren (comp, compName) {
+  let result = []
+  let children = comp.$children
+  if (children && children.length) {
+    for (let child of children) {
+      if (child.$options.name === compName) {
+        result.push(child)
+      } else {
+        result.concat(findChildren(child, compName))
+      }
+    }
+  }
+  return result
+}
+
+export function findBrothers (comp, compName, exceptCur) {
+  let result = comp.$parent.$children.filter(item => item.$options.name === compName)
+  if (exceptCur) result = result.filter(item => item._uid !== comp._uid)
+  return result
+}
