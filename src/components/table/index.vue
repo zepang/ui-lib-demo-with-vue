e8eb030bf047a1cfdf00a123dfd1487f649271ba
zepang <ummlq4@gmail.com>
2019-02-28 16:53:13 +0800

    给table组件添加slot-scope的写法

--- src/components/table/index.vue
+++ src/components/table/index.vue
@@ -12,6 +12,9 @@
           <template v-if="'render' in col">
             <render :row="row" :column="col" :index="rowIndex" :render="col.render"></render>
           </template>
+          <template v-else-if="'slot' in col">
+            <slot :row="row" :column="col" :index="rowIndex" :name="col.slot"></slot>
+          </template>
           <template v-else>
             {{ row[col.key] }}
           </template>
